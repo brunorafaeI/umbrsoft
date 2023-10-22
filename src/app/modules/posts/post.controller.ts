8f6ed7f9454c00c6e5838720ab403657747aeb36
Bruno Rafael <brunorafaelap@gmail.com>
2023-10-22 23:22:21 +0200

    fix: remove poc with post controller

--- src/app/modules/posts/post.controller.ts
+++
@@ -1,9 +0,0 @@
-import { Controller, Get } from '@/common/decorators/route'
-
-@Controller('/posts')
-export class PostController {
-  @Get('/')
-  public postIndex (_, res): any {
-    return res.status(200).send({ message: 'List of posts' })
-  }
-}
