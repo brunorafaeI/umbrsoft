7dc518ceb91db2c51e06ca56921cabef1c1f65d4
Bruno Rafael <brunorafaelap@gmail.com>
2023-12-17 12:08:48 +0100

    feat: add controller to auth login

---
+++ src/app/modules/auth/login-controller.ts
@@ -0,0 +1,17 @@
+import { Controller, Post } from '@/common/decorators/route'
+import { type Users } from '@/persistences/typeorm/models/access/Users'
+import { type FastifyRequest } from 'fastify'
+
+interface LoginFormRequest extends FastifyRequest {
+  body: {
+    credential: Pick<Users, 'email' | 'password'>
+  }
+}
+
+@Controller('/auth')
+export class LoginController {
+  @Post('/login')
+  async login (req: LoginFormRequest, res): Promise<any> {
+    return res.status(200).send({ body: req.body })
+  }
+}
